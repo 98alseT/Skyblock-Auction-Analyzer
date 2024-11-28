7c0fe493fb7e8222afdcaae062e4556c1d74f92a
Tesla <168594870+98alseT@users.noreply.github.com>
2024-11-28 13:44:13 +0100

    Sakuplja unikatne iteme (nedovrseno)
    Imao sam mali kancer i nisam zavrsio takodje ne izgleda lepo ali to je problem za kasnije.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 #Imports
 import os
+import re
 import shutil
+import json
 from Ah import AuctionHouse
+from collections import defaultdict
 
 #Functions
 def deleteTemp():
@@ -11,12 +14,29 @@ def deleteTemp():
 
 def jsonWrite(file, content):
     os.makedirs("temp", exist_ok=True)
-    with open("temp/"+file,'w') as f:
-        json.dump(content, f, indent=4)
+    with open("temp/"+file+".json",'w') as f:
+        json.dump(content, f, indent=2)
 
 #Variables
 auctions = AuctionHouse()
 
 #Code
+deleteTemp()
 auctions.scrape()
 print(auctions.size())
+
+svi_nazivi_itema = defaultdict(lambda: defaultdict(dict))
+
+for i in auctions.list_of_items:
+    s = i["item_name"]
+    s = re.sub(r'[^\x00-\x7F]+', '', s)
+
+    if s[0] == '[':
+        s = s[s.find(']')+2:].strip()
+    
+    s = s.lstrip()
+
+    svi_nazivi_itema[i["category"]][s] = True
+
+for key, val in svi_nazivi_itema.items():
+    jsonWrite(key, val)
